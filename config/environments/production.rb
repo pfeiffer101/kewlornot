deb882e63e8b377102ac380a3f585089f57362d4
Marc Pfeiffer <marcpfeiffer@gmail.com>
2015-04-29 12:27:05 -0700

    Changed AWS to S3 for bucket in production.rb environments file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host:'kewlornot.com', port: 3000 }
 
   # Sets Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
+   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-
 end
