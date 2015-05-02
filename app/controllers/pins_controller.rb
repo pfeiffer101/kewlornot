5bb8fc30de3b0ebb623713c2ab154a3188b07587
Marc Pfeiffer <marcpfeiffer@gmail.com>
2015-05-01 18:58:01 -0700

    Corrected sequence of before_actions in the pins.controller.rb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -42,15 +42,13 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-
     end
-
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
