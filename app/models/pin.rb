81430c1017f7abcc70a0c3361487dbc17aa7faa7
Marc Pfeiffer <marcpfeiffer@gmail.com>
2015-05-03 16:37:27 -0700

    made missing.jpg images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
     class Pin < ActiveRecord::Base
        attr_accessor :image_file_name
        belongs_to :user
-       has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+       has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
        validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ # As suggested here https://github.com/thoughtbot/paperclip
        # What I had previously "validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]"    	
     end
