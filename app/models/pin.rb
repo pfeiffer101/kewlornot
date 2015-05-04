5795d96c4eb8950fd533938cacd8935b3eae27df
Marc Pfeiffer <marcpfeiffer@gmail.com>
2015-05-04 00:54:00 -0700

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@
        has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
        validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
        # As suggested here https://github.com/thoughtbot/paperclip
+       validates :description, presence: true
+       validates :image, presence: true
     end
