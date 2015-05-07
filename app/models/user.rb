a649b7e50a0481b03a31ad9923d8b8dfa6a65224
Marc Pfeiffer <marcpfeiffer@gmail.com>
2015-05-06 19:34:34 -0700

    Added test HTML to About Us page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :pins, dependent: :destroy 
+  has_many :authentications
 
   validates :name, presence: true
 end
